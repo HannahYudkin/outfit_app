af220469451a67b89fe716d7f4489d39951d9ada
danfrenette <dan.r.frenette@gmail.com>
2019-10-05 13:40:06 -0400

    Users can create articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,24 @@
 class ArticlesController < ApplicationController
-    def new
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.valid?
+      @article.save
+      flash[:notice] = "Article created successfully"
+    else
+      flash[:alert] = "Article not created successfully"
     end
+
+    render new_article_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:name)
+  end
 end
